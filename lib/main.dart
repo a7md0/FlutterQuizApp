ea3f73d9e6ebe1fe3d158e77187870e8404de782
a7md0 <a7m3d699@gmail.com>
2021-01-22 20:59:45 +0300

    Implement stateful and show questions/buttons

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,54 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  int _questionIndex = 0;
+
   @override
   Widget build(BuildContext context) {
+    List<String> questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Quiz App'),
         ),
-        body: Text('Default text!'),
+        body: Column(
+          children: [
+            Text(questions[_questionIndex]),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: _answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: _answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: _answerQuestion,
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex += 1;
+    });
+
+    print('Answer chosen');
   }
 }
