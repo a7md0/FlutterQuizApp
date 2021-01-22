c63fa97f5ee975dd1425b26a66484e985e0f1c8d
a7md0 <a7m3d699@gmail.com>
2021-01-22 18:32:15 +0300

    Add simple appbar with body

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Quiz App'),
+        ),
+        body: Text('Default text!'),
+      ),
     );
   }
 }
